61dbcfdee55881a06b6cd3adaf359e61559467cf
Saju <sajukhan12905@gmail.com>
2025-09-25 18:54:44 +0600

    ebook library management

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "Thank You | DS EBooks",
+  title: "Thank You",
   description:
     "ধন্যবাদ! আপনি সফলভাবে আমাদের ইবুক ডাউনলোড ফর্ম পূরণ করেছেন। DS EBooks এ আপনার যাত্রা শুরু হলো আরও সহজ এবং আনন্দদায়কভাবে।",
 };
